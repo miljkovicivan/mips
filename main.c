a5ef3d5fa8bd6eebc365c67e2d0aeff77e6f2c85
ivan <miljkovicivann@gmail.com>
2017-09-13 03:17:47 -0700

    mips

--- main.c
+++ main.c
@@ -11,12 +11,15 @@ int outputChar = 0;
 int X_REGISTER = 0x10;
 int Y_REGISTER = 0x11;
 
+unsigned int t = 0;
+unsigned int s = 0;
+
 int i2c_err_Wr = 0;
 int i2c_err_Rd = 0;
 
 // X-coordinate
 int pointer_Buffer[2] =  {0x10, 0x00};
-signed short X = 0;
+signed short X = 1;
 signed short Y = 0;
 signed short X_output = 0;
 signed short Y_output = 0;
@@ -32,8 +35,23 @@ sbit LCD_D7 at GPIOA_ODR.B6;
 // LCD output
 char text[16]    = {'*','*','*','*','W','E','L','C', 'O','M','E','*','*','*','*', '*'};
 char angle_Txt[16]    = {'*','*','*','*','W','E','L','C', 'O','M','E','*','*','*','*', '*'};
+char error_text[16]    = {'*','*','*','*','E','R','R','O', 'R','*','*','*','*','*','*', '*'};
 
+unsigned mask = 4096;
 // User interface
+
+     void welcomeMessage()
+{
+  Lcd_Out(1,1,text);
+  Lcd_Out(2,1,text);
+}
+
+void error(){
+     Lcd_Cmd(_LCD_CLEAR);
+     Lcd_Out(1,1,error_text);
+     Delay_ms(100);
+     Lcd_Cmd(_LCD_CLEAR);
+}
 void LCD_setup()
 {
   GPIO_Digital_Output(&GPIOB_BASE, _GPIO_PINMASK_8 | _GPIO_PINMASK_9);  // RS & EN
@@ -41,7 +59,7 @@ void LCD_setup()
   GPIO_Digital_Output(&GPIOB_BASE, _GPIO_PINMASK_0);                    // D5
   GPIO_Digital_Output(&GPIOA_BASE, _GPIO_PINMASK_5 | _GPIO_PINMASK_6);  // D6 & D7
 
-  GPIO_Digital_Output(&GPIOE_BASE, _GPIO_PINMASK_8);
+  GPIO_Digital_Output(&GPIOE_BASE, _GPIO_PINMASK_8 | _GPIO_PINMASK_12);
   GPIO_Digital_Input(&GPIOE_BASE, _GPIO_PINMASK_7);
 
   Lcd_Init();
@@ -50,6 +68,22 @@ void LCD_setup()
   Lcd_Cmd(_LCD_CURSOR_OFF);
   Lcd_Cmd(_LCD_CLEAR);
 }
+
+//Timer2 Prescaler :1; Preload = 59999; Actual Interrupt Time = 1 ms
+
+//Place/Copy this part in declaration section
+void InitTimer2(){
+  RCC_APB1ENR.TIM2EN = 1;
+  TIM2_CR1.CEN = 0;
+  TIM2_PSC = 1;
+  TIM2_ARR = 59999;
+  NVIC_IntEnable(IVT_INT_TIM2);
+  TIM2_DIER.UIE = 1;
+  TIM2_CR1.CEN = 1;
+}
+
+
+
 
 
 void Joystick_read()
@@ -66,8 +100,11 @@ void Joystick_read()
     i2c_err_Wr = 0;
 
     I2C2_Start();
+
     i2c_err_Wr = I2C2_Write(0x40, &X_REGISTER, 1,END_MODE_RESTART);  // read X_axis
+
     I2C2_Read(0x40, &pointer_Buffer, 1,END_MODE_STOP);  // read X_axis
+           error();
     X = (signed short)(pointer_Buffer[0]);
     if(X>=0)
     {
@@ -96,7 +133,9 @@ void Joystick_read()
 
 void convert_Angle()
 {
+
    int angle = 0;
+   Lcd_Chr(1,1,'A');
    angle_Txt[12] = 0x20;
    if(X_output >= MAX_READ - 1){
       angle = 90;
@@ -158,14 +197,12 @@ void display()
 
 void setup()
 {
+  //InitTimer2();
   I2C2_Init();
   LCD_setup();
 }
-void welcomeMessage()
-{
-  Lcd_Out(1,1,text);
-  Lcd_Out(2,1,text);
-}
+
+
 void main()
 {
   setup();
@@ -174,15 +211,43 @@ void main()
   Lcd_Cmd(_LCD_CLEAR);
   Delay_ms(100);
 
+
+  Joystick_read();
+  display();
+
   while (1)
   {
-      Joystick_read();
-      Delay_ms(50);
-      display();
-      Delay_ms(100);
-      wellcomeMessage();
-      Lcd_Cmd(_LCD_CLEAR);
-      Delay_ms(100);
-
+      //Joystick_read();
+      //Delay_ms(500);
+      //display();
+      //Delay_ms(500);
+      //welcomeMessage();
+      //Delay_ms(500);
+      //Lcd_Cmd(_LCD_CLEAR);
   }
+}
+
+void Timer2_interrupt() iv IVT_INT_TIM2 {
+  TIM2_SR.UIF = 0;
+  t+=10;
+
+  //Joystick_read();
+      //Delay_ms(500);
+      //display();
+      //Delay_ms(500);
+
+  if (t == 500 && s==0) {
+     t=0;
+     s=1;
+//     welcomeMessage();
+     //Joystick_read();
+     display();
+  }
+  if (t == 500 && s==1) {
+     t=0;
+     s=0;
+     Lcd_Cmd(_LCD_CLEAR);
+     //Joystick_read();
+  }
+  //Enter your code here
 }
